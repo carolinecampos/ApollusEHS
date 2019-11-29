8f8f2df071633d6c8c231dc80dd186c471ec7c2a
Caroline Silva <caroline.silva@nexxera.com>
2019-11-29 15:32:18 -0300

    adicionando cors

--- apollusehs-core/src/main/java/br/com/carolcampos/controller/AuthenticateController.java
+++ apollusehs-core/src/main/java/br/com/carolcampos/controller/AuthenticateController.java
@@ -2,6 +2,7 @@ package br.com.carolcampos.controller;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.carolcampos.model.Usuario;
 import br.com.carolcampos.services.AuthenticateServices;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/autenticar")
 public class AuthenticateController {
