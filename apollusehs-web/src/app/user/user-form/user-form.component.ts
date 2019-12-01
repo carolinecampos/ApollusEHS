5e763160e5d5bea15574390773f40f80951cda92
Caroline Campos <carolinecampos.softwareengineer@gmail.com>
2019-12-01 20:04:11 -0300

    CRUD

--- apollusehs-web/src/app/user/user-form/user-form.component.ts
+++ apollusehs-web/src/app/user/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  voltar() {
+    this.router.navigate(['/user']);
   }
 
   toEdit(user) {
